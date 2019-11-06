bb9935040c0cce1586fc136c0a0b55b414eb27c3
isabellawhitfield <270026136@bun2-206-08.acg.ac.nz>
2019-11-06 15:00:47 +1300

    objects

---
+++ js/script.js
@@ -0,0 +1,199 @@
+var fabric = [
+    {
+        name: "Bear Meadow",
+        size: 112,
+        colour: "White",
+        price: 17,
+        type: "Cotton Jersey",
+        description: "Organic Cotton Jersey is another great 100% cotton fabric that has a natural stretch. Breathable, lightweight and soft on skin, this range finds its uses in children's garments, t-shirts, sleepwear and more. The Bear Meadow Printed Organic Cotton Jersey Fabric measures a 112 cm in width and is sold by the metre. ",
+        image: ""
+
+
+
+    }
+    {
+        name: "Planes",
+        size: 112,
+        colour: "Red",
+        price: 16,
+        type: "Cotton Jersey",
+        description: "Featuring beautiful planes from a different era, this Planes Printed Organic Cotton Jersey fabric is one of a kind fabric. Made from 100% cotton, this fabric would the soft and breathable one for all your dressmaking ideas. Make a shift dress, t-shirt or leggings and breeze through your day with comfort!",
+        image: "images/redPlane.jpg"
+    }
+    {
+        name: "Watermelon",
+        size: 112,
+        colour: "Pink",
+        price: 16,
+        type: "Cotton Jersey",
+        description: "Warmer days always call for comfort and also happy prints. The comb cotton range of fabric is the perfect choice to make your summer inspired apparel or other projects. The Watermelon Printed Comb Cotton, made from cotton is ideal to create the summer look. Create comfort and style for you this season with the colourful prints.",
+        image: "images/pinkWatermelon.jpg"
+
+    }
+    {
+        name: "Fruit Bowl",
+        size: 148,
+        colour: "White",
+        price: 21,
+        type: "Cotton Jersey",
+        description: "Made from 100% organic cotton, this Fruit Bowl Printed Jersey Fabric is another great fabric for this summer. Jersey is brilliant knitted fabric, light weight fabric and soft on skin and has a natural stretch. Ideal for use in children's garments, sleepwear, t-shirts, this fabric is perfect for comfortable summer outfits.",
+        image: "images/ivoryFruit.jpg"
+
+    }
+    {
+        name: "Dachshund",
+        size: 112,
+        colour: "Blue",
+        price: 16,
+        type: "Cotton Jersey",
+        description: "At Spotlight we have a brilliant range of Comb Cotton Fabrics. The Dachshund Printed Fabrics is light weight, soft on the skin and with a natural stretch. Perfect prints for use in children's garments, t-shirts, sleepwear, this fabric is the one for the warmer season. Sold by the metre, this fabric measures 112 cm in width.",
+        image:"images/blueDachshund"
+    }
+    {
+        name: "Pineapple",
+        size: 148,
+        colour: "Yellow",
+        price: 21,
+        type: "Cotton Jersey",
+        description: "Jocelyn Proust at Spotlight features a stunning range of native Australian prints and colours. The Pineapples Printed Organic Cotton Jersey Fabric is made from 100% cotton, light weight fabric and soft on skin and has a natural stretch. Ideal for use in children's garments, sleepwear, t-shirts, this fabric is perfect for comfortable summer outfits.",
+        image: "images/yellowPineapple.jpg"
+
+
+    }
+    {
+        name: "Rainbow Fish",
+        size: 112,
+        colour: "White",
+        price: 10,
+        type: "Cotton",
+        description: "The Mix N Match TC School Fabric is one of the many new additions to the printed cotton fabric collection now available at Spotlight. Measuring 110 cm wide, make your choice today and add this wonderful selection to start creating your next dazzling project, or browse the full range for plenty more spectacular options.",
+        image: "images/whiteRainbowFish.jpg"
+    }
+    {
+        name: "Cherries",
+        size: 112,
+        colour: "Pink",
+        price: 6,
+        type: "Cotton",
+        description: "The Mix N Match TC Cherries Fabric is one of the many new additions to the printed cotton fabric collection now available at Spotlight. Measuring 112 cm wide, make your choice today and add this wonderful selection to start creating your next dazzling project, or browse the full range for plenty more spectacular options.",
+        image: "images/pinkCherries.jpg"
+        
+    }
+    {
+        name: "Toy Story",
+        size: 112,
+        colour: "Blue",
+        price: 18,
+        type: "Cotton",
+        description: "This Disney Toy Story Toys & Stars Allover Cotton Fabric is the perfect fabric for your little one. Made from pure cotton, this cotton measures 112 cm wide and is available to purchase by the metre. Make a quilt, cushion cover or pillow case out of this fabulous printed cotton! Mix and match your fabrics with the collection of prints available in the Toy Story range.",
+        image: "images/navyToyStory.jpg"
+
+    }
+    {
+        name: "Gingham",
+        size: 148,
+        colour: "Pink",
+        price: 11,
+        type: "Cotton",
+        description: "The Gingham Wide Cotton will add a creative touch to your next sewing or craft project.",
+        image: "images/pinkGingham.jpg"
+
+    }
+    {
+        name: "SleepyFox",
+        size: 112,
+        colour: "Blue",
+        price: 23,
+        type: "Cord",
+        description: "The new prints added to the printed pinwale cord range is nothing short of cute. The fox prints are so pretty, you have to have them in your collection! Part of the finest cord fabric, they are made of 100% cotton. The Shy Fox Printed Cord is a beautiful fabric to create some unique outfits. They are great for some pants, skirts and jacket ideas. Measuring 112 cm in width, they are sold by the metre.",
+        image: "blueFox.jpg"
+    }
+    {
+        name: "Bear and Fox",
+        size: 112,
+        colour: "Blue",
+        price: 23,
+        type: "Cord",
+        description: "There are seriously good prints added to the cord range. The Woodside Printed Pinwale Cord fabric is the latest to the array. Made from 100% cotton, they are brilliant for sewing beautiful outfits.",
+        image: "images/blueFoxBear.jpg"
+
+
+    }
+    {
+        name: "Ducks",
+        size: 112,
+        colour: "Purple",
+        price: 12,
+        type: "Cord",
+        description: "Made from cotton and measuring 112 cm in width, this fabric is sold by the metre, offering fantastic versatility as can purchase the required amount tailored to your fashion requirements! Check out the full range today for many more attractive styles of fabric now available from Spotlight instore and online for all occasions.",
+        image: "images/purpleDucks.jpg"
+    }
+    {
+        name: "Floral",
+        size: 112,
+        colour: "Yellow",
+        price: 23,
+        type: "Cord",
+        description: "Cord is a different and elegant range of fabrics for your projects, Made from 100% cotton, they are another option to include cotton in your wardrobe with pretty prints and colours. Popular for skirts, trousers and jackets, they make great outfits. The Mono Floral Printed Pinwale Cord fabric is a beautiful addition to the range. Measuring 112 cm in width, purchase this beautiful mustard colour fabric by the metre.",
+        image: "images/yellowFloral.jpg"
+        
+    }
+    {
+        name: "Shy Fox",
+        size: 112,
+        colour: "White",
+        price: 23,
+        type: "Cord",
+        description: "The cutest prints are part of the new addition to the printed pinwale cord range. Part of the finest cord fabric, they are made of 100% cotton. The Sleepy Fox Printed Cord is a beautiful fabric to create some unique outfits. They are great for some pants, skirts and jacket ideas. Measuring 112 cm in width, they are sold by the metre.",
+        image: "images/whiteShyFox.jpg"
+
+    }
+    {
+        name: "Stripe",
+        size: 150,
+        colour: "Blue",
+        price: 17,
+        type: "Canvas",
+        description: "Supremely versatile, the Stripe Cotton Canvas will add a light layer of softness and comfort to your furnishing creations. This canvas fabric is ideal for textile decor, soft furnishings and occasional upholstery. Mix and match with the stunning range of canvas fabrics online.",
+        image: "images/blueStripe.jpg"
+    }
+    {
+        name: "Jungle",
+        size: 150,
+        colour: "Green",
+        price: 18,
+        type: "Canvas",
+        description: "Get your outdoors DIY Projects in order with this Jungle Water Resistant Canvas Fabric. This fabric is tested to ensure it is water resistant and light resistant for a minimum of 100 hours. It is ideal for both outdoor and indoor DIY upholstery projects. Shop our entire upholstery fabric range in-store or online at Spotlight today!",
+        image: "images/greenJungle.jpg"
+
+    }
+    {
+        name: "Honeycomb",
+        size: 150,
+        colour: "Blue",
+        price: 18,
+        type: "Canvas",
+        description: "Get your outdoors DIY Projects in order with this Honeycomb Water Resistant Canvas Fabric. This fabric is tested to ensure it is water resistant and light resistant for a minimum of 100 hours. It is ideal for both outdoor and indoor DIY upholstery projects. Shop our entire upholstery fabric range in-store or online at Spotlight today!",
+        image: "images/blueHoneycomb.jpg"
+    }
+    {
+        name: "Dogs",
+        size: 112,
+        colour: "White",
+        price: 14,
+        type: "Canvas",
+        description: "The Lots A Dogs Printed Canvas will add a creative touch to your next sewing or craft project.",
+        image: "images/whiteDogs.jpg"
+    }
+    {
+        name: "Cuboid",
+        size: 150,
+        colour: "Pink",
+        price: 18,
+        type: "Canvas",
+        description: "Get your outdoors DIY Projects in order with this Cuboid Water Resistant Canvas Fabric. This fabric is tested to ensure it is water resistant and light resistant for a minimum of 100 hours. It is ideal for both outdoor and indoor DIY upholstery projects. Shop our entire upholstery fabric range in-store or online at Spotlight today!",
+        image: "images/pinkCuboid.jpg"
+    }
+    
+
+]
